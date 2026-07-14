6f117dea378c7c3302bc75a550f004600dc80dfd
uttamvajapara-a16y <uttamvajapara@gmail.com>
2026-07-14 21:54:37 +0530

    render image

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://employee-management-system-u6n9.onrender.com',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 })
